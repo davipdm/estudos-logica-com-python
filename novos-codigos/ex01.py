60f6e6207804821f8ce43e65ec31f8194e3169b5
Davi Mendes <53956176+davipdm@users.noreply.github.com>
2026-09-07 08:20:30 -0300

    Add string and list variables in ex01.py

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,3 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+minha_string = "Olá, mundo!"
+minha_lista = ["Python", "JavaScript", "C++", "Java"]
